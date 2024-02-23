1bb588c03523c4e750fe9ca2b954ed0f4a5ece7f
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-23 20:02:19 +0530

    chore: adding cors and jwt

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const versionRouter = require("./routes/index");
 require("./db");
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 app.use("/api/v1", versionRouter);
 
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.send("Server Working");
+});
