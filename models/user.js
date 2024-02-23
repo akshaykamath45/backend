061a5e46521d85a1222054ebfe20a45d97e91d68
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-23 19:50:30 +0530

    feat: created user model

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+    maxLength: 50,
+  },
+  points: {
+    type: Number,
+    default: 0,
+  },
+  gitHublink: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 5,
+  },
+});
+
+userSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+userSchema.set("toJSON", {
+  virtuals: true,
+});
+
+userSchema.methods.createHash = async function (plainTextPassword) {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(plainTextPassword, salt);
+};
+
+userSchema.methods.validatePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
