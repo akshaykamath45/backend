075d64e645a1a5d87a40a71c74f57c63ef59fc9d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-24 20:22:11 +0530

    feat: implementing CRUD operations for job listing

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const userRouter = require("./user");
-
+const jobRouter = require("./job");
 const versionRouter = express.Router();
 
 versionRouter.use("/user", userRouter);
+versionRouter.use("/job", jobRouter);
 
 module.exports = versionRouter;
--- routes/job.js
+++ routes/job.js
@@ -1,4 +1,87 @@
 const express = require("express");
+const JobListing = require("../models/job");
+
 const jobRouter = express.Router();
+
+jobRouter.post("/", async (req, res) => {
+  try {
+    const { date, link, title, usersApplied } = req.body;
+    const newJobListing = new JobListing({
+      date,
+      link,
+      title,
+      usersApplied,
+    });
+    const savedJobListing = await newJobListing.save();
+    res.status(201).json({
+      message: "Job listing created successfully",
+      jobListing: savedJobListing,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: "Bad Request" });
+  }
+});
+
+jobRouter.get("/", async (req, res) => {
+  try {
+    const jobListings = await JobListing.find();
+    res.json({ jobListings });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+jobRouter.get("/:id", async (req, res) => {
+  try {
+    const job = await JobListing.findById(req.params.id);
+    if (!job) {
+      return res.status(404).json({ message: "Job listing not found" });
+    }
+    res.json({ job });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+jobRouter.put("/:id", async (req, res) => {
+  try {
+    const { date, link, title, usersApplied } = req.body;
+
+    const updatedJobListing = await JobListing.findByIdAndUpdate(
+      req.params.id,
+      { date, link, title, usersApplied },
+      { new: true }
+    );
+    if (!updatedJobListing) {
+      return res.status(404).json({ message: "Job listing not found" });
+    }
+    res.json({
+      message: "Job listing updated successfully",
+      jobListing: updatedJobListing,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ error: "Bad Request" });
+  }
+});
+
+jobRouter.delete("/:id", async (req, res) => {
+  try {
+    const deletedJobListing = await JobListing.findByIdAndDelete(req.params.id);
+    if (!deletedJobListing) {
+      return res.status(404).json({ message: "Job listing not found" });
+    }
+    res.json({
+      message: "Job listing deleted successfully",
+      jobListing: deletedJobListing,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = jobRouter;
