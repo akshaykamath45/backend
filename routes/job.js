c381945bd76150abd1709fd45232ea0ac6b36eac
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-02-24 22:33:44 +0530

    feat: adding user to a job

--- routes/job.js
+++ routes/job.js
@@ -4,6 +4,7 @@ const JobListing = require("../models/job");
 const jobRouter = express.Router();
 const authMiddleware = require("../middleware");
 
+// create a job
 jobRouter.post("/", authMiddleware, async (req, res) => {
   try {
     const { date, link, title, usersApplied } = req.body;
@@ -19,21 +20,21 @@ jobRouter.post("/", authMiddleware, async (req, res) => {
       jobListing: savedJobListing,
     });
   } catch (error) {
-    console.error(error);
     res.status(400).json({ error: "Bad Request" });
   }
 });
 
+// find all jobs
 jobRouter.get("/", async (req, res) => {
   try {
     const jobListings = await JobListing.find();
     res.json({ jobListings });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// find a job
 jobRouter.get("/:id", authMiddleware, async (req, res) => {
   try {
     const job = await JobListing.findById(req.params.id);
@@ -42,15 +43,14 @@ jobRouter.get("/:id", authMiddleware, async (req, res) => {
     }
     res.json({ job });
   } catch (error) {
-    console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// update a job
 jobRouter.put("/:id", authMiddleware, async (req, res) => {
   try {
     const { date, link, title, usersApplied } = req.body;
-
     const updatedJobListing = await JobListing.findByIdAndUpdate(
       req.params.id,
       { date, link, title, usersApplied },
@@ -64,11 +64,11 @@ jobRouter.put("/:id", authMiddleware, async (req, res) => {
       jobListing: updatedJobListing,
     });
   } catch (error) {
-    console.error(error);
     res.status(400).json({ error: "Bad Request" });
   }
 });
 
+// delete a job
 jobRouter.delete("/:id", authMiddleware, async (req, res) => {
   try {
     const deletedJobListing = await JobListing.findByIdAndDelete(req.params.id);
@@ -80,7 +80,34 @@ jobRouter.delete("/:id", authMiddleware, async (req, res) => {
       jobListing: deletedJobListing,
     });
   } catch (error) {
-    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// add user to a job
+jobRouter.post("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    if (!userId) {
+      return res.status(400).json({ error: "User Id required" });
+    }
+    const jobListing = await JobListing.findById(id);
+    if (!jobListing) {
+      return res.status(404).json({ error: "Job listing not found" });
+    }
+    if (jobListing.usersApplied.includes(userId)) {
+      return res
+        .status(400)
+        .json({ error: "User already applied for this job" });
+    }
+    jobListing.usersApplied.push(userId);
+    await jobListing.save();
+    res.json({
+      message: "User added to the list of applicants successfully",
+      jobListing,
+    });
+  } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
